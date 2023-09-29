009cae7a97a26dc191a75e043a70e3f9c592b5ae
Catalina Maldonado <142262435+CatalinanMe@users.noreply.github.com>
2023-09-29 12:15:56 -0300

    Update TareaRepository.java

--- src/main/java/com/example/tarea/Repositories/TareaRepository.java
+++ src/main/java/com/example/tarea/Repositories/TareaRepository.java
@@ -1,4 +1,6 @@
 package com.example.tarea.Repositories;
+import com.example.tarea.Models.Tarea;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
 }
