50e123471992c138658312b00000ddaaeea36d95
Catalina Maldonado <142262435+CatalinanMe@users.noreply.github.com>
2023-09-29 12:15:34 -0300

    Update ComentarioRepository.java

--- src/main/java/com/example/tarea/Repositories/ComentarioRepository.java
+++ src/main/java/com/example/tarea/Repositories/ComentarioRepository.java
@@ -1,4 +1,6 @@
 package com.example.tarea.Repositories;
+import com.example.tarea.Models.Comentario;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
 }
